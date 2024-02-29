c387c622e2db4c444858081c6c73ce6c93e326c9
Sarvesh <sarveshjaiofficial@gmail.com>
2024-02-29 20:33:06 +0530

    bug fix

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,9 +10,10 @@ import {
 } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300;
+export const maxDuration = 9;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+
 export async function GET() {
   try {
     connectToDB();
